a5fddf968b391a0df73d293a02a2e70dcbd911a6
lear23 <130910243+lear23@users.noreply.github.com>
2024-03-26 05:49:28 +0100

    fixad subscriber

--- SiliconWebApp/Controllers/HomeController.cs
+++ SiliconWebApp/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SiliconWebApp.Controllers;
 
@@ -6,6 +10,7 @@ public class HomeController : Controller
 {
     public IActionResult Home()
     {
+
         return View();
     }
 
@@ -15,4 +20,33 @@ public class HomeController : Controller
     }
 
 
+
+
+
+    //public IActionResult Subscribe()
+    //{
+    //    ViewData["Subscribed"] = false;
+    //    return View();
+    //}
+
+
+
+    [HttpPost]
+    public async Task<IActionResult> Home(SubscriberEntity model)
+    {
+        if (ModelState.IsValid)
+        {
+            using var http = new HttpClient();
+            var json = JsonConvert.SerializeObject(model);
+            using var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await http.PostAsync($"https://localhost:7071/api/Subscriber?email={model.Email}", content);
+            if (response.IsSuccessStatusCode)
+            {
+                ViewData["Subscribed"] = true;
+
+                return RedirectToAction("Home");
+            }
+        }
+        return View(); 
+    }
 }
--- WebAppApi/Controllers/SubscriberController.cs
+++ WebAppApi/Controllers/SubscriberController.cs
@@ -1,12 +1,113 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAppApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class SubscriberController : ControllerBase
+public class SubscriberController(AppDbContext context) : ControllerBase
 {
+    private readonly AppDbContext _context = context;
 
+    #region CREATE
+    [HttpPost]
+    public async  Task<IActionResult> Create(string email)
+    {
+        if (!string.IsNullOrEmpty(email))
+        {
+            if (!await _context.Subscrivers.AnyAsync(x => x.Email == email))
+            {
+                try
+                {
+                    var subscriberEntity = new SubscriberEntity { Email = email };
+                    _context.Subscrivers.Add(subscriberEntity);
+                    await _context.SaveChangesAsync();
+
+                    return Created("", null);
+                }
+                catch
+                {
+                    return Problem("Unable to create subscription");
+                }
+            }
+            return Conflict("This email address is already subscribed");
+        }
+        return BadRequest();
+    }
+
+    #endregion
+
+
+    #region GET
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var subscriber = await _context.Subscrivers.ToListAsync();  
+        if (subscriber.Count != 0)
+        {
+            return Ok(subscriber);
+        }
+
+        return NotFound();
+    }
+
+    [HttpGet ("{email}")]
+    public async Task<IActionResult> GetOne(string email)
+    {
+        var subscriber = await _context.Subscrivers.FirstOrDefaultAsync(x => x.Email == email);
+
+        if (subscriber != null)
+        {
+            return Ok(subscriber);
+        }
+        return NotFound();
+    }
+
+    #endregion
+
+
+    #region Update
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, string email)
+    {
+        var subscriber = await _context.Subscrivers.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (subscriber != null)
+        {
+
+            subscriber.Email = email;
+            _context.Subscrivers.Update(subscriber);
+            await _context.SaveChangesAsync();
+
+            return Ok(subscriber);
+        }
+        return NotFound();
+    }
+
+    #endregion
+
+
+    #region DELETE
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var subscriber = await _context.Subscrivers.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (subscriber != null)
+        {
+          
+            _context.Subscrivers.Remove(subscriber);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        return NotFound();
+    }
+
+    #endregion
 
 }
